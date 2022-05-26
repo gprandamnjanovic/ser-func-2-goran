6b6953a394de6e0eaa5a1f7c6a9c3cd3d9a85d98
damnjanovicgoran <damnjanovicgoran7@yahoo.com>
2022-05-26 15:06:44 +0200

    coors-fix-error

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,6 +3,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      'Access-Control-Allow-Orgin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
